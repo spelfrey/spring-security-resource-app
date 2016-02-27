2f334fd9662e0e8a1a6e2e7722109a709bdf09a7
Stephen Pelfrey <pelfrey.stephen@gmail.com>
2016-02-27 12:15:33 -0600

    CORS Filter added

---
+++ src/main/java/com/example/CorsFilter.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by stephen.pelfrey on 2/27/16.
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter{
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		if (request.getMethod() != "OPTIONS") {
+			filterChain.doFilter(request, response);
+		} else {
+
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
--- src/main/java/com/example/ResourceAppApplication.java
+++ src/main/java/com/example/ResourceAppApplication.java
@@ -2,9 +2,24 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
 public class ResourceAppApplication {
+
+	@RequestMapping("/")
+	public Map<String,Object> home() {
+		Map<String,Object> model = new HashMap<>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceAppApplication.class, args);
